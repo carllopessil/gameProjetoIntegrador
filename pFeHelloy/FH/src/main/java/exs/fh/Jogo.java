c796a8fd973e10841674b677830a1236c4f3bfd4
HeloeFe <>
2022-05-23 16:09:02 -0300

    HELOeFellipe

---
+++ pFeHelloy/FH/src/main/java/exs/fh/Jogo.java
@@ -0,0 +1,30 @@
+package exs.fh;
+
+public class Jogo {
+
+    public static void main(String[] args) {
+        System.out.println("Tudo começa em uma festa no Olimpo, onde dois Deuses, Ártemis e Ares se apaixona e acaba tendo um filho chamado Theo. ");
+        System.out.println("Isso causa uma grande revolta em Zeusa que acaba,"
+                + " mandando o Ares para enfrentar os Titãs.");
+        System.out.println("Onde Theo acaba perdendo seu pai na guerra contra os Titãs, isso acaba deixando os Deuses\n"
+                + "do Olimpo furiosos com Zeus, para que Zeus não levasse toda a culpa, ele acaba tendo");
+        System.out.println("uma solução.");
+        System.out.println("Onde ele resolve colocar Theo para assumir o lugar de seu pai.");
+        System.out.println("Só que Theo ainda era criança e precisava de treinamento para assumir esse grande posto,\n" +
+"então Zeus resolve que Ártemis que deverá cuidar de Theo e dar todos os ensinamentos\n" +
+"para que Ele além de se tornar um Deus ele tivesse uma visão diferente do que seu pai.\n" +
+"Só que a Ártemis não daria conta de dar todos os ensinamentos para Theo.");
+        System.out.println("Athena vendo toda aquela situação ela resolve ajudar, dando ensinamentos sobre estratégia e alguns ensinamentos sobre Guerra.");
+        System.out.println("Em uns dos ensinamentos de Athena ela acaba mando Theo para terra onde ele terá que\n" +
+"passar por um soldado que tem que usar a estratégia para pode escapar");
+        System.out.println("-  Bom Theo, no treinamento de hoje vou contar uma história de um soldado onde\n" +
+"você terá que sair dessa situação usando alguns treinamentos que você aprendeu");
+        System.out.println("- Tudo bem, estou pronto");
+        System.out.println("Então Athena começa a contar a história.");
+        //FH.main(args);
+        System.out.println("Theo acaba passando pelo treinamento de Athena obtendo sabedoria e conhecimento\n" +
+"sobre as guerra");
+        
+        
+    }
+}
