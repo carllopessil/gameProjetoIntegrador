d2bca08aa4b14315fa7d9db5957598efe324b70d
HeloeFe <>
2022-05-23 17:14:35 -0300

    Versão jogo completo.

--- pFeHelloy/FH/src/main/java/exs/fh/Jogo.java
+++ pFeHelloy/FH/src/main/java/exs/fh/Jogo.java
@@ -11,7 +11,8 @@ public class Jogo {
         System.out.println("uma solução.");
         System.out.println("Onde ele resolve colocar Theo para assumir o lugar de seu pai.");
         System.out.println("Só que Theo ainda era criança e precisava de treinamento para assumir esse grande posto,\n" +
-"então Zeus resolve que Ártemis que deverá cuidar de Theo e dar todos os ensinamentos\n" +
+"então Zeus resolve q"
+                + "ue Ártemis que deverá cuidar de Theo e dar todos os ensinamentos\n" +
 "para que Ele além de se tornar um Deus ele tivesse uma visão diferente do que seu pai.\n" +
 "Só que a Ártemis não daria conta de dar todos os ensinamentos para Theo.");
         System.out.println("Athena vendo toda aquela situação ela resolve ajudar, dando ensinamentos sobre estratégia e alguns ensinamentos sobre Guerra.");
@@ -22,9 +23,31 @@ public class Jogo {
         System.out.println("- Tudo bem, estou pronto");
         System.out.println("Então Athena começa a contar a história.");
         //FH.main(args);
+     carloEgu.main(args); 
         System.out.println("Theo acaba passando pelo treinamento de Athena obtendo sabedoria e conhecimento\n" +
 "sobre as guerra");
+        System.out.println("Tempos depois, acaba recebendo o treinamento de sua mãe onde ela irá ensinar\n" +
+"sobre a humanidade os animais e a natureza");
+        System.out.println("Passar do anos Theo com 20 anos acaba recebendo seu último treinamento de sua mãe\n" +
+"onde ele terá que passar por um índio que tem que objetivo de usar os ensinamentos de\n" +
+"Athena e aprender um pouco sobre a natureza");
+        System.out.println("Sim, estou pronto");
+        FH.main(args);
+        System.out.println("Theo é chamado por Zeus para ganhar a aprovação para se tornar um Deus.");
+        System.out.println("Ele se depara com os três principais Deuses como Zeus, Hades e Poseidon .");
+        System.out.println("-Olá Theo, parece que você está pronto para passar pela aprovação");
         
+        System.out.println("-Você enfrentará os três grande monstro do Olimpo");
+        System.out.println("Minotauro que representa Hades , Equidna que representa Poseidon e Grifo que\n" +
+"representa Zeus");
+        FelipeEJoao.main(args);
+        System.out.println("Parabéns Theo, você está preparado para se tornar um Deus e assumir o posto de seu pai");
+        
+        
+        
+     
+             
+         
         
     }
 }
